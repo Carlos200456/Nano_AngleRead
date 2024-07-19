debaf4dfa72f309c0e899f5cd3786aaa35a71b3f
Carlos Surface <carlos@meditech.com.ar>
2024-07-19 19:35:42 -0300

    Funcionando sin limites reales

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 #include <EEPROM.h>
+#include <U8g2lib.h>
 
-#define IDIup 2
-#define IDIdw 3
+U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+
 #define CArco A0
 #define CRota A1
 #define SDD A2
@@ -15,22 +16,38 @@ String ACK ="ACK";
 String NACK ="NACK";
 bool DataReady = false;             // whether the string is complete
 bool error = false;                 // whether the string have errors
-unsigned long lastDebounceTimeSC = 0;  // the last time the output SC was toggled
-unsigned long debounceDelaySC = 20;    // the debounce time; increase if the output flickers
-unsigned long lastDebounceTimeCI = 0;  // the last time the output CI was toggled
-unsigned long debounceDelayCI = 20;    // the debounce time; increase if the output flickers
-int buttonStateSC;                     // the current reading from the input pin
-int buttonStateCI;                     // the current reading from the input pin
-int lastButtonStateSC = 0;           // the previous reading from the input pin SC
-int lastButtonStateCI = 0;           // the previous reading from the input pin CI
 int eeAddress; //EEPROM address to start reading from
+
+int CangleX, LangleX, TangleX, Tang, pasos = 0;
+float Cang, Lang, CangF, LangF;
+
+
+void ReadAnalogVal(void){
+  int test;
+  test = analogRead(CArco);
+  CangleX = analogRead(CArco);
+  test = analogRead(CRota);
+  LangleX = analogRead(CRota);
+  test = analogRead(Techo);
+  TangleX = analogRead(Techo);
+}
+
+
+
 void setup() {
   // initialize serial:
-  Serial.begin(57600);
+  Serial.begin(9600);                 // Start the serial terminal
+  
+  u8x8.begin();  // initialize with the I2C
+  u8x8.setPowerSave(0);
+  // init done
+  u8x8.setFont(u8x8_font_pxplustandynewtv_r);  // u8x8_font_torussansbold8_r, u8x8_font_chroma48medium8_r, u8x8_font_victoriamedium8_r, u8x8_font_pxplustandynewtv_r
+  u8x8.draw1x2String(0,0,"Cartech");
+  u8x8.setCursor(0,2);             // Column, Row
+  u8x8.print("GEO Angle Read");
+
   // reserve 20 bytes for the inputString:
   inputString.reserve(20);
-  pinMode(IDIup, INPUT_PULLUP);
-  pinMode(IDIdw, INPUT_PULLUP);
   pinMode(CArco, INPUT);
   pinMode(CRota, INPUT);
   pinMode(SDD, INPUT);
@@ -39,9 +56,59 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  ReadAnalogVal();
+
+  Cang = map(CangleX, 0, 1023, -90, 90); // Map the analog value to degrees
+  Lang = map(LangleX, 0, 1023, -90, 90); // Map the analog value to degrees
+  Tang = map(TangleX, 0, 1023, 0, 180); // Map the analog value to degrees
+
+  // Low-pass filter
+  CangF = 0.95 * CangF + 0.05 * Cang;
+  LangF = 0.95 * LangF + 0.05 * Lang;
 
 
 
+
+  if (pasos == 40){
+    u8x8.setCursor(0,3);             // Column, Row
+    u8x8.print("    C     L     ");
+    u8x8.setCursor(0,4);             // Column, Row
+    u8x8.print("                ");
+    if (CangF >= 0) {
+      u8x8.setCursor(3,4);             // Column, Row
+      u8x8.print("+");
+      u8x8.setCursor(4,4);             // Column, Row
+      u8x8.print(CangF, 0);
+    } else {
+      u8x8.setCursor(3,4);             // Column, Row
+      u8x8.print(CangF, 0);
+    }
+    if (LangF >= 0) {
+      u8x8.setCursor(9,4);             // Column, Row
+      u8x8.print("+");
+      u8x8.setCursor(10,4);             // Column, Row
+      u8x8.print(LangF, 0);
+    } else {
+      u8x8.setCursor(9,4);             // Column, Row
+      u8x8.print(LangF, 0);
+    }
+    if (Tang >= 0) {
+      u8x8.setCursor(6,5);             // Column, Row
+      u8x8.print("+");
+      u8x8.setCursor(7,5);             // Column, Row
+      u8x8.print(Tang);
+    } else {
+      u8x8.setCursor(6,5);             // Column, Row
+      u8x8.print(Tang);
+    }
+
+    if (CangF >= 0) Serial.print("  "); else Serial.print(" "); Serial.print(CangF, 0); if (abs(CangF) < 10) Serial.print(" "); Serial.print(",");
+    if (LangF >= 0) Serial.print("  "); else Serial.print(" "); Serial.print(LangF, 0); if (abs(LangF) < 10) Serial.print(" "); 
+    Serial.print(", 0000, 0000, XXX, XXX,"); 
+    Serial.println();
+    pasos = 0;
+  } else pasos += 1;
+  delay(10);
 }
 
 /*
